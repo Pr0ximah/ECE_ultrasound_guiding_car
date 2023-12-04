821a98ddbd54f7094e23cb20b26b9bf5cf28d46c
Pr0ximah <51467731+Pr0ximah@users.noreply.github.com>
2023-12-04 19:29:52 +0800

    init: first setting

---
+++ include/portDef.h
@@ -0,0 +1,10 @@
+#ifndef _PORT_DEF_H
+#define _PORT_DEF_H
+
+// Motor Port
+extern int PortMotorA1 = 9;
+extern int PortMotorB1 = 10;
+extern int PortMotorA2 = 11;
+extern int PortMotorB2 = 12;
+
+#endif // _PORT_DEF_H
